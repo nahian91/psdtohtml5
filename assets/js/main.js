0e6d95c6a6682e735b7024b092692120a5af6210
nahian28 <nahian.instructory@gmail.com>
2023-03-08 03:26:40 +0600

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
     // Counter Up
     $('.counter').counterUp();
+
+    // Magnific Popup
+    $('.popup-image').magnificPopup({
+        type:'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    // Testimonials Slider
+    $('.testimonials').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        loop: true
+    });
+
+    // Clients Slider
+    $('.clients').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    });
 })
